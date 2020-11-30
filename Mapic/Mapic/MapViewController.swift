cb489affa9defc2740780bf66f7e369209e2bfa7
jawaheralaggl <jawaheralaggl@gmail.com>
2020-11-30 15:11:05 +0300

    configure location services

--- Mapic/Mapic/MapViewController.swift
+++ Mapic/Mapic/MapViewController.swift
@@ -10,15 +10,46 @@ import MapKit
 
 class MapViewController: UIViewController {
     
+    // MARK: - Properties
+
     var manager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus() //TODO: find replacement
 
     @IBOutlet weak var mapView: MKMapView!
     
+    
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        manager.delegate = self
+        configureLocationServices()
+        
     }
+
+    // MARK: - Helpers
 
 
 }
 
+// MARK: - MKMapViewDelegate
+
+extension MapViewController: MKMapViewDelegate {
+    
+    
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension MapViewController: CLLocationManagerDelegate {
+        
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            manager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+}
