02a0b768ac7bf547c25d1c9048fb48927ece542e
jawaheralaggl <jawaheralaggl@gmail.com>
2020-11-30 16:50:41 +0300

    show user location

--- Mapic/Mapic/MapViewController.swift
+++ Mapic/Mapic/MapViewController.swift
@@ -11,39 +11,61 @@ import MapKit
 class MapViewController: UIViewController {
     
     // MARK: - Properties
-
+    
     var manager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus() //TODO: find replacement
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // map view properties
         mapView.delegate = self
-        manager.delegate = self
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        mapView.showsUserLocation = true
+        
         configureLocationServices()
         
+        // location manager properties
+        if CLLocationManager.locationServicesEnabled() {
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.startUpdatingLocation()
+        }
+        
     }
-
+    
     // MARK: - Helpers
-
-
+    
+    
 }
 
 // MARK: - MKMapViewDelegate
 
 extension MapViewController: MKMapViewDelegate {
     
+    // set the region to center the map on user location
+    func zoomInUserLocation() {
+        guard let coordinate = manager.location?.coordinate else { return }
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000 , longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+    }
     
 }
 
 // MARK: - CLLocationManagerDelegate
 
 extension MapViewController: CLLocationManagerDelegate {
-        
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        zoomInUserLocation()
+    }
+    
     func configureLocationServices() {
         if authorizationStatus == .notDetermined {
             manager.requestAlwaysAuthorization()
