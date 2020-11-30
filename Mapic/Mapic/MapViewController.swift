06f19c38b6724de205521f5c7f0974e310ef20ba
jawaheralaggl <jawaheralaggl@gmail.com>
2020-11-30 14:35:13 +0300

    add map

--- Mapic/Mapic/MapViewController.swift
+++ Mapic/Mapic/MapViewController.swift
@@ -1,14 +1,19 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Mapic
 //
 //  Created by Jawaher Alagel on 11/30/20.
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
+    
+    var manager = CLLocationManager()
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
