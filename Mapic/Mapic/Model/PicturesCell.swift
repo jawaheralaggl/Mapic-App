b670cea4bc78440f2d620245daeed9061e8f5c04
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-01 15:26:00 +0300

    add properties to the cell

--- Mapic/Mapic/Model/PicturesCell.swift
+++ Mapic/Mapic/Model/PicturesCell.swift
@@ -8,11 +8,37 @@
 import UIKit
 
 class PicturesCell: UICollectionViewCell {
- 
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 12
+        return iv
+    }()
+    
+    let distanceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textColor = .white
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
         
+        contentView.addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        imageView.addSubview(distanceLabel)
+        distanceLabel.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 60).isActive = true
+        distanceLabel.leftAnchor.constraint(equalTo: imageView.leftAnchor, constant: 8).isActive = true
+        distanceLabel.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
