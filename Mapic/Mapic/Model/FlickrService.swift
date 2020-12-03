6df341298a078cd53054c38a6bbd4b94e060444b
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-03 16:06:53 +0300

    add Alamofire library

---
+++ Mapic/Mapic/Model/FlickrService.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrService.swift
+//  Mapic
+//
+//  Created by Jawaher Alagel on 12/3/20.
+//
+
+import Foundation
+import Alamofire
+import AlamofireImage
+
+class FlickrService {
+    static let shared = FlickrService()
+    
+    
+    
+}
