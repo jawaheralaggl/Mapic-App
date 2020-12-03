bf98617759e8eea0fafebb14132d2c47170e7da1
jawaheralaggl <jawaheralaggl@gmail.com>
2020-12-03 16:29:20 +0300

    create api url

--- Mapic/Mapic/Model/FlickrService.swift
+++ Mapic/Mapic/Model/FlickrService.swift
@@ -14,7 +14,17 @@ class FlickrService {
     
     // MARK: - Properties
 
-
+    var picUrlArray = [String]()
+    var pictureArray = [UIImage]()
+    
+    let baseURLString = "https://api.flickr.com/services/rest"
+    let endPoint = "flickr.photos.search"
+    let apiKey = "cc109f8c6934c41b7cf7baf0555c2ef1"
+    
+    func flickrURL(apiKey key: String, pinAnnotation annotation: Pin, numberOfPhotos number: Int) -> String {
+        let url =  "\(baseURLString)/?method=\(endPoint)&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=km&per_page=\(number)&format=json&nojsoncallback=1"
+        return url
+    }
     
     // MARK: - Helpers
 
